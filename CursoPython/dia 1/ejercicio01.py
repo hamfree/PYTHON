d50f93ad13e16e92bb8bccad65a692dcb175bd7d
Juan F. Ruiz Fernandez <juanfco.ruiz@gmail.com>
2019-12-05 19:20:35 +0100

    Se adapta el ejercicio01 para Python 3 y se agrega el tutorial de python 3 del creador del lenguaje

--- CursoPython/dia 1/ejercicio01.py
+++ CursoPython/dia 1/ejercicio01.py
@@ -16,7 +16,7 @@ def validaDni(dni):
             and tabla[int(dni)%23] == dig_control
     return False
 
-print "Ejercicio final\n"
+print ("Ejercicio final\n")
 valido = False
 esNombreValido = False
 esApellidoValido = False
@@ -30,50 +30,50 @@ persona = {'nombre':'','apellidos':'','edad':0,'dni':'','peso':0,'altura':0}
 # Recogida de datos
 while not valido:
     while not esNombreValido:
-        entrada = raw_input('Nombre:')
+        entrada = input('Nombre:')
         if len(entrada) > 0:
             persona['nombre'] = entrada
             esNombreValido = True
         else:
-            print "El nombre es obligatorio"
+            print("El nombre es obligatorio") 
     while not esApellidoValido:
-        entrada = raw_input('Apellidos:')
+        entrada = input('Apellidos:')
         if len(entrada) > 0:
             persona['apellidos'] = entrada
             esApellidoValido = True
         else:
-            print "El apellido es obligatorio"
+            print("El apellido es obligatorio") 
     while not esEdadValida:
-        entrada = raw_input('Edad:')
+        entrada = input('Edad:')
         numero = int(entrada)
         if numero > 0 and numero < 100:
             persona['edad'] = int(entrada)
             esEdadValida = True
         else:
-            print "La edad debe estar entre 0 y 100"
+            print("La edad debe estar entre 0 y 100") 
     while not esDNIValido:
-        entrada = raw_input('DNI:')
+        entrada = input('DNI:')
         esDNIValido = validaDni(entrada)
         if esDNIValido:
             persona['dni'] = entrada
         else:
-            print "El DNI no es válido"
+            print("El DNI no es válido")
     while not esPesoValido:
-        entrada = raw_input('Peso:')
+        entrada = input('Peso:')
         numero = int(entrada)
         if numero > 35 and numero < 125:
             persona['peso'] = int(entrada)
             esPesoValido = True
         else:
-            print "El peso debe estar entre 35 y 125"
+            print("El peso debe estar entre 35 y 125")
     while not esAlturaValida:
-        entrada = raw_input('Altura:')
+        entrada = input('Altura:')
         numero = int(entrada)
         if numero > 50 and numero < 250:
             persona['altura'] = int(entrada)
             esAlturaValida = True
             valido = True
         else:
-            print "La altura debe estar entre 50 y 250 cm"
+            print("La altura debe estar entre 50 y 250 cm")
 
-print persona
+print(persona)
